c6e04476db80ccfddc94dede51453697bb10041b
Anurag <anuraggoutam01@gmail.com>
2023-02-21 12:43:25 +0530

    userCreate

--- frontend/src/screens/SignupScreen.js
+++ frontend/src/screens/SignupScreen.js
@@ -3,16 +3,19 @@ import { useLocation, Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import Axios from 'axios';
+import Axios, { toFormData } from 'axios';
 import { useContext, useEffect, useState } from 'react';
 import { Store } from '../Store';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { geterror } from '../utilis';
-const SigninScreen = () => {
+
+const SignUpScreen = () => {
   const navigate = useNavigate();
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const { search } = useLocation(); // give current url
   const redirectInUrl = new URLSearchParams(search).get('redirect'); // URLSearchParams give url as a object // get give value of object key
@@ -23,9 +26,12 @@ const SigninScreen = () => {
 
   const submitHandler = async (e) => {
     e.preventDefault();
-    
+    if (password !== confirmPassword) {
+      toast.error('Passwords do not match');
+    }
     try {
-      const { data } = await Axios.post('/api/users/signin', {
+      const { data } = await Axios.post('/api/users/signup', {
+        name,
         email,
         password,
       });
@@ -45,10 +51,14 @@ const SigninScreen = () => {
   return (
     <Container className="small-container">
       <Helmet>
-        <title>Sign In</title>
+        <title>Sign Up</title>
       </Helmet>
-      <h1 className="my-3">Sign In</h1>
+      <h1 className="my-3">Sign Up</h1>
       <Form onSubmit={submitHandler}>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control required onChange={(e) => setName(e.target.value)} />
+        </Form.Group>
         <Form.Group className="mb-3" controlId="email">
           <Form.Label>Email</Form.Label>
           <Form.Control
@@ -65,16 +75,24 @@ const SigninScreen = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
         </Form.Group>
+        <Form.Group className="mb-3" controlId="confirmpassword">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            required
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </Form.Group>
         <div className="mb-3">
-          <Button type="submit">Sign In</Button>
+          <Button type="submit">Sign Up</Button>
         </div>
         <div className="mb-3">
-          New costumer{' '}
-          <Link to={`/signup?redirect=${redirect}`}>Create your account</Link>
+          Already have an account{' '}
+          <Link to={`/signin?redirect=${redirect}`}>Sign In</Link>
         </div>
       </Form>
     </Container>
   );
 };
 
-export default SigninScreen;
+export default SignUpScreen;
